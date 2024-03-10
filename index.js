c38e07983218258060a13220a39cb7fd9963221f
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-03-10 13:49:42 +0100

    deleting commented code

--- index.js
+++ index.js
@@ -11,28 +11,23 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, ...data }) {
   switch (action) {
     case "list":
       const allContacts = await contactsMethods.listContacts();
       return console.table(allContacts);
-      //break;
 
     case "get":
       const oneContact = await contactsMethods.getContactById(id);
       return console.log(oneContact);
-      //break;
 
     case "add":
       const newContact = await contactsMethods.addContact(data);
       return console.log(newContact);
-      //break;
 
     case "remove":
       const deleteContact = await contactsMethods.removeContact(id);
       return console.log(deleteContact);
-      //break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
